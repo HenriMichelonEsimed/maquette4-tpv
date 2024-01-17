3653dff8028bcfb2c640d200b4684107a0231c2c
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2024-01-17 22:25:41 +0100

    holy cows 2

--- game/models/props/exteriors/cow_2/cow_2.gd
+++ game/models/props/exteriors/cow_2/cow_2.gd
@@ -1,10 +1,10 @@
 extends StaticBodyAnimated
 
-@onready var mesh:MeshInstance3D = $Sketchfab_model/ec391e5c533447c599096ccc677f3a8f_fbx/Object_2/RootNode/Object_4/Skeleton3D/Object_7
+@onready var mesh:MeshInstance3D = $"Sketchfab_model/ec391e5c533447c599096ccc677f3a8f_fbx/Object_2/RootNode/Object_4/Skeleton3D/0"
 
 func _ready():
 	super._ready()
-	var mat:ShaderMaterial = mesh.get_active_material(0).duplicate()
+	var mat:ShaderMaterial = mesh.get_surface_override_material(1).duplicate()
 	mat.set_shader_parameter("seed", Vector2(randf()*100, randf()*100))
-	mesh.set_surface_override_material(0, mat)
+	mesh.set_surface_override_material(1, mat)
 	pass
