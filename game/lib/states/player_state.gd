c879ba53cf4fb13599afde3f3dfd3674fd9af596
HenriMichelon <148531229+HenriMichelon@users.noreply.github.com>
2024-01-19 10:54:59 +0100

    export

--- game/lib/states/player_state.gd
+++ game/lib/states/player_state.gd
@@ -1,6 +1,6 @@
 class_name PlayerState extends State
 
-var zone_name:String = "level_1/level_1"
+var zone_name:String = "level_test/level_test"
 var position:Vector3 = Vector3.ZERO
 var rotation:Vector3 = Vector3.ZERO
 var current_item_type:Item.ItemType = Item.ItemType.ITEM_UNKNOWN
